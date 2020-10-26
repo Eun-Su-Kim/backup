27aa74091612cc4024c79b64e9daebc38e57ab7b
Eun-Su-Kim <50953323+Eun-Su-Kim@users.noreply.github.com>
2020-10-27 01:27:00 +0900

    python 배우기 6

---
+++ boj/10000~19999/10869.cpp
@@ -0,0 +1,13 @@
+//사칙연산
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  cout << a - b << endl;
+  cout << a * b << endl;
+  cout << a / b << endl;
+  cout << a % b << endl;
+}
