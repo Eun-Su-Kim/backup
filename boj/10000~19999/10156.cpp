530d3bfe7b821d12e7371ff023f640ffecb1ed66
Eun-Su-Kim <50953323+Eun-Su-Kim@users.noreply.github.com>
2020-11-13 02:38:21 +0900

    python 배우기 31

---
+++ boj/10000~19999/10156.cpp
@@ -0,0 +1,13 @@
+//과자
+#include <iostream>
+using namespace std;
+
+int main() {
+  int k, n, m;
+  cin >> k >> n >> m;
+  if (k * n <= m) {
+    cout << 0 << endl;
+  } else {
+    cout << k * n - m << endl;
+  }
+}
