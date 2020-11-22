308fc39cd628a3fe653068ba81f49b300a29b434
Eun-Su-Kim <50953323+Eun-Su-Kim@users.noreply.github.com>
2020-11-22 22:37:52 +0900

    python 배우기 42

---
+++ boj/0000~9999/5717.cpp
@@ -0,0 +1,12 @@
+//상근이의 친구들
+#include <iostream>
+using namespace std;
+
+int main() {
+  while (1) {
+    int m, f;
+    cin >> m >> f;
+    if (m == 0 && f == 0) break;
+    cout << m + f << endl;
+  }
+}
