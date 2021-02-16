6bcc19757c49132b4191f358f7a78a262d1767e2
Eun-Su-Kim <50953323+Eun-Su-Kim@users.noreply.github.com>
2021-02-16 23:01:50 +0900

    python 배우기 75

---
+++ boj/9325.cpp
@@ -0,0 +1,20 @@
+//얼마?
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int price;
+    cin >> price;
+    int n;
+    cin >> n;
+    while (n--) {
+      int q, p;
+      cin >> q >> p;
+      price += q * p;
+    }
+    cout << price << '\n';
+  }
+}
