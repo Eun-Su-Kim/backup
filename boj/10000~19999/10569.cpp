1341b3c54b4ed6d3a8eb23b85389c935cc8a30a3
Eun-Su-Kim <50953323+Eun-Su-Kim@users.noreply.github.com>
2021-02-21 22:26:59 +0900

    python 배우기 81

---
+++ boj/10000~19999/10569.cpp
@@ -0,0 +1,13 @@
+//¥Ÿ∏È√º
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int v, e;
+    cin >> v >> e;
+    cout << 2 - v + e << '\n';
+  }
+}
